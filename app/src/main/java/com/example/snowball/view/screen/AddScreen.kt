3de3aa964f8bf2a8e4fde812484ebde293e51443
patrache <ssk5442@naver.com>
2022-11-11 02:20:36 +0900

    add screen making

--- app/src/main/java/com/example/snowball/view/screen/AddScreen.kt
+++ app/src/main/java/com/example/snowball/view/screen/AddScreen.kt
@@ -1,29 +1,65 @@
 package com.example.snowball.view.screen
 
+import android.widget.EditText
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun addScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
+    var text by remember { mutableStateOf("") }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Text(
-            text = "This is Add",
-            style = MaterialTheme.typography.bodyLarge,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(top = 80.dp)
+        ) {
+
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                label = { Text(text = "포트폴리오 이름") },
+                singleLine = true,
+                placeholder = { Text(text = "포트폴리오 이름을 입력하세요.") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(red = 243, green = 243, blue = 250)
+                )
+            )
+
+            Row(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            ) {
+                Spacer(modifier = Modifier.width(280.dp))
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 16.dp),
+                    text = "전략 총 0 개",
+                    fontSize = 18.sp
+                )
+            }
+
+            Divider(
+                modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                thickness = 2.dp,
+                color = Color(0x66, 0xC6, 0xA3)
+            )
+
+        }
     }
 }
